4fd6cfb091f224803d1657fc76c3ce8bb9d62c76
MichaelFelixChan <michaelfelixchandra_2327240030@mhs.mdp.ac.id>
2025-05-08 09:55:18 +0700

    perubahan v4

--- pendaftaran_ukm/koneksi.php
+++ pendaftaran_ukm/koneksi.php
@@ -5,3 +5,8 @@ $pass="";
 $dbname = "db_ukm";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname) or die("Database tidak terhubung");
+
+if ($conn -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn -> connect_error;
+    exit();
+  }
